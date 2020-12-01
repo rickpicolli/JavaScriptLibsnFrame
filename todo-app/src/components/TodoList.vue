9d50e194b9d78b1ec440aab92c3566b0f93c53bd
Ricardo Alexandre Pinheiro Picolli <rickpicolli@Mac.local>
2020-12-01 15:48:57 -0800

    adding vue homework

---
+++ todo-app/src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+   // we are now passing the data to the todo component to render the todo list
+    <todo  v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo"></todo>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+
+import Todo from './Todo';
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo,
+  },
+};
+</script>
+<style>
+
+</style>
