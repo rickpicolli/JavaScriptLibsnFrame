d7b5b86bee7fcd36c482f0f1f1ab4c739896667e
Ricardo Alexandre Pinheiro Picolli <rickpicolli@Mac.local>
2020-12-02 15:47:49 -0800

    creating vue homework

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <TodoList/> -->
+    <!-- <todo-list></todo-list> -->
+    <Todos />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld'
+import Todos from './components/TodoList'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Todos,
+  },
+  data() {
+    return {
+      todos: [
+      {
+        id: 1,
+        title: 'Go workout',
+        completed: false
+      }, {
+        id: 2,
+        title: 'Do laundry',
+        completed: false
+      }, {
+        id: 3,
+        title: 'Cook food',
+        completed: false
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false
+      }],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ todo-app/src/components/TodoList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+      <!-- <p>Completed Tasks: {{ todo.done === true }} </p> -->
+      <!-- <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p> -->
+      <!-- <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p> -->
+      <!-- <todo v-for="todo in todos" v-bind:todo="todo" :key="todo"></todo> -->
+
+      <h2>My todolist </h2>
+
+
+
+
+
+
+
+      <!-- <div class="ui centered card" v-for="todo in todos" :key="todo.id">
+          <div class="content">
+              <div class="header">
+                  {{ todo.title }}
+              </div>
+              <div class="meta">
+                  {{ todo.project }}
+              </div>
+              <div class="extra content">
+                  <span class="right floated edit icon">
+                      <i class="edit icon"></i>
+                  </span>
+              </div>
+          </div>
+          <div class="ui bottom attached green basic button" v-show="todo.done">
+              Completed
+          </div>
+          <div class="ui bottom attached red basic button" v-show="!todo.done">
+              Complete
+          </div>
+      </div> -->
+
+
+
+
+<!-- 
+    <ul>
+        <li> Todo A </li> 
+        <li> Todo B </li> 
+        <li> Todo C </li> 
+    </ul>  -->
+  </div>
+</template>
+
+<script type = "text/javascript" >
+
+import Todo from './Todo';
+
+export default {
+    // props: ['todos'],
+    name: 'Todos',
+    components: {
+        Todo,
+    },
+};
+</script>
+<style>
+</style>
